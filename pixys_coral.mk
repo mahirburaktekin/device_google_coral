2d462e8839d40c4ef2c593eee2e40ff5e97d7466
aswin7469 <aswinas@pixysos.com>
2023-12-21 23:44:21 +0530

    floral: inherit apex modules
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- pixys_coral.mk
+++ pixys_coral.mk
@@ -54,7 +54,9 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_SUPPORTS_NOW_PLAYING := true
 
+$(call inherit-product, vendor/mainline_modules/config.mk)
 $(call inherit-product, device/google/coral/device-coral.mk)
 $(call inherit-product-if-exists, vendor/google_devices/coral/proprietary/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/coral/prebuilts/device-vendor-coral.mk)
