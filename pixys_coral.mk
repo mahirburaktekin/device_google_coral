c76a690d8a161479811baabf917816342d4ff831
aswin7469 <aswinas@pixysos.com>
2024-01-12 18:44:32 +0530

    Revert "floral: inherit apex modules"
    This reverts commit 2d462e8839d40c4ef2c593eee2e40ff5e97d7466.
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- pixys_coral.mk
+++ pixys_coral.mk
@@ -53,9 +53,7 @@ TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_NOW_PLAYING := true
 
-$(call inherit-product, vendor/mainline_modules/config.mk)
 $(call inherit-product, device/google/coral/device-coral.mk)
 $(call inherit-product-if-exists, vendor/google_devices/coral/proprietary/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/coral/prebuilts/device-vendor-coral.mk)
